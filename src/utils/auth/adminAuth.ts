02271c3f6da9a1a94460b49c65fe2313e59cca62
Ritesh Newal <riteshnewal@hotmail.com>
2025-09-04 11:35:10 +0530

    new admin ceated

--- src/utils/auth/adminAuth.ts
+++ src/utils/auth/adminAuth.ts
@@ -27,13 +27,23 @@ export const handlePredefinedAdminLogin = (identifier: string, password: string)
     };
     localStorage.setItem('currentUser', JSON.stringify(admin));
     return admin;
+  }else if (identifier === 'ronak@jokova.com' && password === 'Ronak@0507') {
+    const admin = {
+      id: '00000000-0000-0000-0000-000000000002',
+      name: 'Super Admin',
+      email: 'ronak@jokova.com',
+      phone: '',
+      role: 'super_admin' as User['role']
+    };
+    localStorage.setItem('currentUser', JSON.stringify(admin));
+    return admin;
   }else if (identifier === 'damini@jokova.com' && password === 'Damini@1234') {
     const admin = {
       id: '00000000-0000-0000-0000-000000000002',
-      name: 'Admin User',
+      name: 'Super Admin',
       email: 'damini@jokova.com',
       phone: '',
-      role: 'admin' as User['role']
+      role: 'super_admin' as User['role']
     };
     localStorage.setItem('currentUser', JSON.stringify(admin));
     return admin;
