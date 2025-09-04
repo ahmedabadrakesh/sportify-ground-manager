c41b9173356d73f16f05153bd8a26f19e91078e2
Ritesh Newal <riteshnewal@hotmail.com>
2025-09-04 11:19:07 +0530

    damini@jokova.com added

--- src/utils/auth/adminAuth.ts
+++ src/utils/auth/adminAuth.ts
@@ -27,6 +27,16 @@ export const handlePredefinedAdminLogin = (identifier: string, password: string)
     };
     localStorage.setItem('currentUser', JSON.stringify(admin));
     return admin;
+  }else if (identifier === 'damini@jokova.com' && password === 'Damini@1234') {
+    const admin = {
+      id: '00000000-0000-0000-0000-000000000002',
+      name: 'Admin User',
+      email: 'damini@jokova.com',
+      phone: '',
+      role: 'admin' as User['role']
+    };
+    localStorage.setItem('currentUser', JSON.stringify(admin));
+    return admin;
   }
   
   return null;
